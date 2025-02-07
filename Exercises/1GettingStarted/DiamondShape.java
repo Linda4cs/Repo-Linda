5a235ff39214d5b3ebdde2195db717a55b04343a
Linda4cs <L.Dike3784@student.leedsbeckett.ac.uk>
2025-02-07 17:19:34 +0000

    add file excersiseanswers

--- Exercises/1GettingStarted/DiamondShape.java
+++ Exercises/1GettingStarted/DiamondShape.java
@@ -1,9 +1,19 @@
 import java.util.Scanner;
 
 public class DiamondShape {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        // Program to  print a diamond shape
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("         I");
+        System.out.println("        her");
+        System.out.println("       where");
+        System.out.println("      collect");
+        System.out.println("       seven");
+        System.out.println("        she");
+        System.out.println("         I");
 
     }
-    // Program to read and print a diamond shape
+
 
 }
---
+++ Exercises/1GettingStarted/ExcersiseAnswers.java
@@ -0,0 +1,15 @@
+public class ExcersiseAnswers {
+    public static void main(String[] args) {
+
+    }
+    //No; a = JDK 23
+
+    //No; b = Java SE is used for developing and deploying portable code foe desktop and server environment. its features include Java Compiler, Java Virtual Machine, Java Run-time Environment Java Development Kit and Class Libraries.
+    //while Java ME is used for developing application for mobile devices and embedded systems. its features include a single SDK for application development, built-in emulators, customizable devices skins, and plugins for the IDE Integration
+
+    // No; c = Windows, macOS, and Linux based operating systems, and for the aarch64 architecture for macOS and Linux
+
+    //No; d = IntelliJ IDEA
+
+    //No; e = Is the starting point from where the JVM starts the execution of a Java program. JVM will not execute the code, if the program is missing the main method.
+}
