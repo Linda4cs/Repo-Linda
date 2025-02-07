c1002defff4b6048cb87bffadad588fe5ac510d8
Linda4cs <L.Dike3784@student.leedsbeckett.ac.uk>
2025-02-07 02:15:37 +0000

    add firstpractical

--- Exercises/1GettingStarted/DiamondShape.java
+++ Exercises/1GettingStarted/DiamondShape.java
@@ -6,12 +6,4 @@ public class DiamondShape {
     }
     // Program to read and print a diamond shape
 
-    System.out.println("        I");
-    System.out.println("       she");
-    System.out.println("      where");
-    System.out.println("     collect");
-    System.out.println("      seven");
-    System.out.println("       his");
-    System.out.println("        a");
-
 }
